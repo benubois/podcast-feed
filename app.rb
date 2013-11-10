5fc2f261a4cb776847eb572cd9257cae2a052e8b
Ben Ubois <ben@benubois.com>
2013-11-09 23:25:54 -0800

    App updates.

--- app.rb
+++ app.rb
@@ -9,17 +9,12 @@ require "time"
 require "./show"
 
 get "/" do
-  erb :index, locals: {
-    shows: get_shows,
-    hostname: request.host
-  }
-end
-
-def get_shows
   shows = []
   Dir.glob("./public/shows/*.mp3").reverse.each do |file|
     shows << Show.new(file)
   end
-  shows
+  erb :index, locals: {
+    shows: shows,
+    hostname: request.host
+  }
 end
-
