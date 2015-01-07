1d35bbc26c50d933c0307b2c02b8948a8725e9bd
Ben Ubois <ben@benubois.com>
2015-01-07 07:47:06 -0800

    Support for multiple shows. Renames.

--- app.rb
+++ app.rb
@@ -6,15 +6,35 @@ require "sinatra"
 require "uri"
 require "time"
 
-require "./show"
+require "./episode"
+
+$shows = {
+  "howard_stern" => {
+    title: "Howard Stern"
+  },
+  "daves_of_thunder" => {
+    title: "Daves of Thunder"
+  }
+}
+
+def build_episodes(show)
+  Dir.glob("./public/shows/#{show}/*.mp3").reverse.each_with_object([]) do |file, array|
+    array << Episode.new(file)
+  end
+end
 
 get "/" do
-  shows = []
-  Dir.glob("./public/shows/*.mp3").reverse.each do |file|
-    shows << Show.new(file)
-  end
   erb :index, locals: {
-    shows: shows,
+    shows: $shows
+  }
+end
+
+get "/show/:show" do
+  content_type 'text/xml'
+  episodes = build_episodes(params[:show])
+  erb :show, locals: {
+    show: $shows.fetch(params[:show]),
+    episodes: episodes,
     hostname: request.host
   }
 end
--- episode.rb
+++ episode.rb
@@ -1,20 +1,20 @@
-class Show
+class Episode
   def initialize(file)
     @file = file
   end
-  
+
   def name
     Pathname.new(@file).basename.sub('.mp3', '').to_s
   end
-  
+
   def url
     @file.sub('./public/', '')
   end
-  
+
   def date
     Time.parse(name).rfc2822
   end
-  
+
   def length
     File.size(@file)
   end
